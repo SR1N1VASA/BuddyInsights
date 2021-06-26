4bffbe411af38d74e6fec5598f7dde66e38188be
SR1N1VASA <srinivasayachitula@gmail.com>
2021-06-25 23:17:54 -0400

    adding structure, googleOauth using passport, mongodb and authentication

--- index.js
+++ index.js
@@ -1,23 +1,41 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const cookieSession = require('cookie-session');
 const passport = require('passport');
-const GoogleStrategy = require('passport-google-oauth20').Strategy;
-const app = express();
 const keys = require('./config/keys');
+const authRoutes = require('./routes/authRoutes');
+require('./models/user');
+require('./services/passport');
 
-passport.use(new GoogleStrategy({
-    clientID: keys.googleClientID,
-    clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
-}, (accessToken) => {
-    console.log(accessToken);
-}));
+mongoose
+  .connect(keys.mongoURI)
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
+const app = express();
+// all api.use calls are middlewares(functions to modify incoming requests before they reach the route handler)
+// extract cookie data from the request(cookie stored in req.session)
+app.use(
+    cookieSession({
+        // lasts for 30 days
+        maxAge: 30 * 24 * 60 * 60 * 1000,
+        // key to encrpyt/decrypt the cookie
+        keys: [keys.cookieKey]
+
+    })
+)
+
+// commands to tell passport to make use of cookies to handle authentication
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./routes/authRoutes')(app);
 
 
-app.get('/auth/google', passport.authenticate('google', {
-    scope: ['profile', 'email']
-}));
-
-app.get('/auth/google/callback', passport.authenticate('google'));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);

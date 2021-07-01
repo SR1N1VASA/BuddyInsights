54a161970bc60c53e29c1d340c9b1e0c2cfd7cad
SR1N1VASA <srinivasayachitula@gmail.com>
2021-07-01 16:50:43 -0400

    Listing the Surveys in the frontend

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { FETCH_USER } from './types';
+import { FETCH_USER, FETCH_SURVEYS } from './types';
 
 // action creator
 // export const fetchUser = () => {
@@ -24,4 +24,9 @@ export const submitSurvey = (values, history) => async dispatch => {
     // navigate to dashboard after saving survey
     history.push('/surveys');
     dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const fetchSurveys = () => async dispatch => {
+    const res = await axios.get('/api/surveys');
+    dispatch({ type: FETCH_SURVEYS, payload: res.data });
 };
